3201b06b3e929ff76fbfb747f9ef2da3d8b57828
IanMacfarlane <iandmacfarlane13@gmail.com>
2020-01-24 18:56:02 +0000

    conway

--- src/main.py
+++ src/main.py
@@ -4,45 +4,18 @@ import time
 #initialize curses and returns window object representing entire screen
 stdscr = curses.initscr()
 
-#terminate curses manually
-#curses.nocbreak()
-#stdscr.keypad(False)
-#curses.echo()
-#restore terminal to original operation mode
-#curses.endwin()
-
 def main(stdscr):
-    #turn off automatic echoing of keys to the screen
-    curses.noecho()
-
-    #allows keyboard input without requiring the enter key to be pressed
-    #curses.cbreak()
-
-    #abstracts keyboard input
-    #stdscr.keypad(True)
-
-    #make cursor invisible
-    #curs_set(False)
-
-    #curses.LINES
-    #curses.COLS
-    #stdscr.addstr('hello world')
-    #stdscr.refresh()
-
-    #time.sleep(5)
-    
-    #refresh screen then wait for keyboard input returns key. getch() returns keycode. getstr()
-    #userInput = stdscr.getkey()
-    #addstr(y,x,str)
-    #stdscr.addstr(str(userInput))
-    #stdscr.getch()
 
     #basic cursor movement
     yMax = curses.LINES
     xMax = curses.COLS
+
+    #initialize 2d list each cell as False
     cells = [[False] * xMax for i in range(yMax)]
+
     y = 0
     x = 0
+
     while(True):
         userInput = stdscr.getch()
 
@@ -86,10 +59,6 @@ def main(stdscr):
         stdscr.move(y,x)
 
 def runGame(stdscr, cells):
-
-    while(True):
-        1
-
 
 #restore terminal to original state when main exits
 curses.wrapper(main)
